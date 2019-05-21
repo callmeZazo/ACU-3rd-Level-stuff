524d3a728c1a557adf167c166dd3484b9841d4da
Zeyad Nofal <37900425+callmeZazo@users.noreply.github.com>
2019-05-21 16:23:07 +0200

    Add files via upload

---
+++ Information & Network Security/AES/src/com/aes/Main.java
@@ -0,0 +1,45 @@
+package com.aes;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Scanner;
+
+public class Main {
+    static Cipher cipher;
+
+    public static String encrypt(String plainText, SecretKey key) throws Exception{
+        byte[] plainTextByte = plainText.getBytes();
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedByte = cipher.doFinal(plainTextByte);
+        Base64.Encoder encode = Base64.getEncoder();
+        String encryptedText = encode.encodeToString(encryptedByte);
+        return encryptedText;
+    }
+
+    public static String decrypt(String encryptedText, SecretKey key) throws Exception{
+        Base64.Decoder decode = Base64.getDecoder();
+        byte[] encryptedTextByte = decode.decode(encryptedText);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decryptedByte = cipher.doFinal(encryptedTextByte);
+        String decryptedText = new String(decryptedByte);
+        return decryptedText;
+    }
+
+    public static void main(String[] args) throws Exception{
+	// write your code here
+        KeyGenerator keygen = KeyGenerator.getInstance("AES");
+        keygen.init(128);
+        SecretKey key = keygen.generateKey();
+        cipher = Cipher.getInstance("AES");
+        Scanner input = new Scanner(System.in);
+
+        String plainText = input.nextLine();
+        String cipheredText = encrypt(plainText, key);
+        String decipheredText = decrypt(cipheredText, key);
+        System.out.println("Plain text: " + plainText);
+        System.out.println("Encrypted text: " + cipheredText);
+        System.out.println("Deciphered text: " + decipheredText);
+    }
+}
